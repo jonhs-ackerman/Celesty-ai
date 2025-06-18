77b92aba96e6506e9f42a5d3561bdba47c313756
jonhs-ackerman <dieumerciciribagula650@gmail.com>
2025-06-18 22:14:06 +0200

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -23,9 +23,9 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			leaveType1: "𝗲𝘀𝘁 𝗮𝗹𝗹𝗲𝗿 𝘀𝗲 𝗽𝗲𝗻𝗱𝗿𝗲 𝗵𝗼𝗿𝘀 𝗱𝘂 𝗴𝗿𝗼𝘂𝗽𝗲 🙃🙎‍♂️",
+			leaveType2: "𝗲𝘀𝘁 𝗺𝗮𝗶𝗻𝘁𝗲𝗻𝗮𝗻𝘁 𝗱𝗲𝗵𝗼𝗿𝘀 𝗴𝗿𝗮𝗰𝗲 𝗮 𝘀𝗼𝗻 𝗲𝘅𝗽𝘂𝘀𝗶𝗼𝗻 𝗽𝗮𝗿 𝘂𝗻 𝗮𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝘁𝗲𝘂𝗿 𝗾𝘂𝗲 𝗷𝗲 𝗰𝗼𝗻𝗻𝗮𝗶𝘀 𝗽𝗮𝘀 🥴 ",
+			defaultLeaveMessage: "{userName} {type} \n☘️ 𝗟𝗔𝗧𝗨𝗠 ☘️"
 		}
 	},
 
